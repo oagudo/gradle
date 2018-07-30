f5842e5049b2351e4b6f650bbdc6b45a3a6bdea3
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2018-07-30 16:02:02 -0300

    Add `Project.task(String, Action<Task>)` overload
    To improve parity between the Groovy and Kotlin DSLs.

--- subprojects/core/src/main/java/org/gradle/api/internal/project/DefaultProject.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/project/DefaultProject.java
@@ -1213,6 +1213,11 @@ public class DefaultProject extends AbstractPluginAware implements ProjectIntern
     }
 
     @Override
+    public Task task(String task, Action<? super Task> configureAction) {
+        return taskContainer.create(task, configureAction);
+    }
+
+    @Override
     public Task task(String task, Closure configureClosure) {
         return taskContainer.create(task).configure(configureClosure);
     }
